0e87721a9e92fbe68cf927da6aa9cec10e35f509
bmargon <bmargon@DAW1-41.dip.local>
2022-12-22 10:13:42 +0100

    (incompleto) Javadocs de la clase MenuGenerator añadidos. Estan todos añadidos pero falta una correción ortografica

--- src/ahorcado/MenuGenerator.java
+++ src/ahorcado/MenuGenerator.java
@@ -9,12 +9,41 @@ package ahorcado;
  * @author Bilo Alejandro Martins Gonzalez
  */
 public class MenuGenerator {
+
+    /**
+     * Mostra por pantalla o menú principal e xenera a palabra que o usuario vai
+     * adiviñar
+     */
+    private String showInitMenu() {
+        WordGenerator word = new WordGenerator();
+        return "a";
+    }
+
+    /**
+     * Este metodo implementa el status del juego, los fallos, aciertos y
+     * tambien permite una cantidad limitada de fallos mientras pide las letras
+     */
+    private void showGameMenu() {
+        System.out.println("");
+    }
+
+    /**
+     * Este metodo pregunta al usuario si quiere seguir jugando o no
+     */
+    private boolean showExitMenu() {
+
+    }
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         MenuGenerator menuGenerator = new MenuGenerator();
-        
+
         do {
             menuGenerator.hangMan = new HangMan(menuGenerator.showInitMenu());
-            menuGenerator.showGameMenu();   
+            menuGenerator.showGameMenu();
         } while (!menuGenerator.showExitMenu());
     }
 }

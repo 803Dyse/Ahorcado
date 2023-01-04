8813fab7712b52cec88f766ef972aa80b5a7f255
SrUsuario <raul.parada040103@gmail.com>
2023-01-04 17:23:49 +0100

    (incompleto) Engadidos comentarios JavaDoc da clase e do atributo

--- src/ahorcado/WordGenerator.java
+++ src/ahorcado/WordGenerator.java
@@ -8,16 +8,23 @@ import java.util.Random;
 
 /**
  *
+ * Esta clase encárgase de xerar a palabra a adiviñar
+ *
  * @author Bilo Alejandro Martins González y Raúl Parada de la Fuente
  */
 public class WordGenerator {
 
+    /**
+     * Array de string que é a lista de palabras posibles entre as que escoller
+     * a palabra oculta
+     */
     public static final String[] WORDLIST = {"patacas", "bateria", "cacao", "luna", "java"};
 
     public String generateWord() {
         Random randomWord = new Random();
         int numberGenerated = randomWord.nextInt(WORDLIST.length);
         String wordGenerated = WORDLIST[numberGenerated];
+        System.out.println("Depuración: " + wordGenerated);
         return wordGenerated;
     }
 }

d96c42614161e595839689e5b4c029d696278474
803Dyse <alexdyse803@gmail.com>
2023-01-05 13:51:18 +0100

    (Incompleto) correcion gramatica

--- src/ahorcado/MenuGenerator.java
+++ src/ahorcado/MenuGenerator.java
@@ -53,7 +53,7 @@ public class MenuGenerator {
         Scanner sc = new Scanner(System.in);
         char userOption = sc.nextLine().toLowerCase().charAt(0);
         if (userOption != 's') {
-            System.out.println("Muchas por jugar al ahorcado :)");
+            System.out.println("Muchas gracias por jugar al ahorcado :)");
             return true;
         }
         return false;

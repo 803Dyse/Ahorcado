2fdaca20d6349dd11348dd9c70af5e5432c03107
SrUsuario <raul.parada040103@gmail.com>
2023-02-28 10:47:15 +0100

    Mejorada la clase GUIKeyboardWordGenerator, segunda versión

--- src/ui/GUIKeyboardWordGenerator.java
+++ src/ui/GUIKeyboardWordGenerator.java
@@ -4,25 +4,45 @@
  */
 package ui;
 
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+
 /**
  *
  * @author Alejandro Martínez Domínguez, Bilo Alejandro Martins González y Raúl
  * Parada de la Fuente
  */
 public class GUIKeyboardWordGenerator implements WordGenerator {
-    
+
     @Override
     public String generateWord() throws GenerateWordException {
-        return "";
+        String customWord = "";
+
+        String word;
+
+        do {
+            JPasswordField pwd = new JPasswordField();
+
+            word = JOptionPane.showInputDialog(pwd, "Palabra", customWord);
+
+        } while (!isValid(word));
+
+        customWord = String.valueOf(word);
+
+        System.out.println("DEBUG!!! PALABRA = " + customWord);
+
+        return customWord;
     }
-    
+
     /**
      * Este método comproba se todas os caracteres de word están en minúscula
-     * 
+     *
      * @param word a palabra a comprobar
      * @return Devolve true o false
      */
     private boolean isValid(String word) {
+        System.out.println(word);
+        System.out.println(word.toLowerCase());
         if (word.equals(word.toLowerCase())) {
             return true;
         }
